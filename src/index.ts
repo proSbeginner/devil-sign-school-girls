57b9d39c396594fe8ce6abac54e0ac99ea5b81f2
kittipong.p <kittipnog.p@arise.tech>
2025-04-07 22:30:06 +0700

    add girl1 walk animation

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,39 @@
-import { Application, Assets, Sprite } from 'pixi.js'
+import { AnimatedSprite, Application, Assets } from 'pixi.js'
 
 (async () => {
-  const appConfig = { background: '#1099bb', resizeTo: window }
+  const appConfig = {
+    // width: window.innerWidth,
+    // height: window.innerHeight,
+    background: 0x1099bb,
+    resizeTo: window,
+    // resolution: window.devicePixelRatio || 1,
+    // autoDensity: true,
+  }
   const app = new Application()
   await app.init(appConfig)
   document.body.appendChild(app.canvas)
-  
-  const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
-  const sprite = new Sprite(texture)
-  sprite.anchor.set(0.5);
-  sprite.x = app.screen.width / 2;
-  sprite.y = app.screen.height / 2;
 
-  app.stage.addChild(sprite)
-  
+  const texture = await Assets.load('assets/girl_1_walk.json')
+  const walkingSprite = AnimatedSprite.fromFrames([
+    "Walk_000", "Walk_001", "Walk_002", "Walk_003",
+    "Walk_004", "Walk_005", "Walk_006", "Walk_007",
+    "Walk_008", "Walk_009", "Walk_010", "Walk_011"
+  ])
+  walkingSprite.animationSpeed = 0.3
+  walkingSprite.play()
+  app.stage.addChild(walkingSprite)
+
+  let direction = 1; // ให้ตัวละครเดินไปทางขวา
   app.ticker.add((time) => {
-    sprite.rotation += 0.1 * time.deltaTime;
+    walkingSprite.x += 1.2 * time.deltaTime * direction;
+
+    // ตรวจสอบขอบจอและเปลี่ยนทิศทาง
+    if (walkingSprite.x > app.screen.width - 50) {
+      direction = -1;
+      walkingSprite.scale.x = -1;
+    } else if (walkingSprite.x < 50) {
+      direction = 1;
+      walkingSprite.scale.x = 1;
+    }
   })
 })()
